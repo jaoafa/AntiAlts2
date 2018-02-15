ad5d1eac8910f3f6a620401ea2dbd47f46e20b44
tomachi <lego@aol.jp>
2018-02-15 16:55:45 +0900

    add: First Commit

---
+++ src/main/java/com/jaoafa/AntiAlts2/AntiAlts2.java
@@ -0,0 +1,25 @@
+package com.jaoafa.AntiAlts2;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class AntiAlts2 extends JavaPlugin {
+	/**
+	 * プラグインが起動したときに呼び出し
+	 * @author mine_book000
+	 * @since 2018/02/15
+	 */
+	@Override
+	public void onEnable() {
+
+	}
+
+	/**
+	 * プラグインが停止したときに呼び出し
+	 * @author mine_book000
+	 * @since 2018/02/15
+	 */
+	@Override
+	public void onDisable() {
+
+	}
+}
